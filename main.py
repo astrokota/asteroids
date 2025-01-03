4067cb8c04dd99bc9cfabfed10a5375b9f7ca592
github_astrokota <bikesdakota@gmail.com>
2025-01-03 18:11:42 +0000

    imported constants to main.py, added print statements to starting message

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+from constants import *
 
 def main():
     
     print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
