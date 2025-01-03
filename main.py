7998bd83819d7942829846c8840bf614ea5bc4b4
github_astrokota <bikesdakota@gmail.com>
2025-01-03 19:22:42 +0000

    added clock/fps

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pygame
 from constants import *
 
 def main():
+
+    print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
@@ -12,10 +19,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    print("Starting asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+        dt = clock.tick(60)
+        dt = dt / 1000
 
 if __name__ == "__main__":
     main()
