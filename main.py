5ae42a22e8bfad8953c31604ddcc3564f537cfd2
github_astrokota <bikesdakota@gmail.com>
2025-01-03 18:29:30 +0000

    added screen GUI functionality and gitignore

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from constants import *
 
 def main():
     
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
